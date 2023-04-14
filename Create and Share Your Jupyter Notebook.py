e52f35101c97034111e9a3b22242560d83be690b
thiagobatistanb <130616432+thiagobatistanb@users.noreply.github.com>
2023-04-13 21:19:39 -0400

    Create and Share Your Jupyter Notebook

---
+++ Create and Share Your Jupyter Notebook.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In[9]:
+
+
+print("In this notebook, Data Science Tools and Ecosystem are summarized.")
+
+
+# #### Some of the popular languages that Data Scientists use are:
+# 1. Python 
+# 2. R
+# 3. SQL
+# 
+# 
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# * Pandas 
+# * NumPy
+# * Keras
+
+# | Data Science Tools|
+# | -------------|
+# | TensorFlow |
+# | RStudio | 
+# | TensorFlow |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[7]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[8]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# ### This course covers important fundamentals in Data Science, for example: 
+# 
+# * How to code using Python
+# * How to use Jupter Notebooks 
+# * The importance of GitHub and RStudio
+
+# # Author
+# 
+# Thiago Bandeira 
+
+# In[ ]:
+
+
+
+
